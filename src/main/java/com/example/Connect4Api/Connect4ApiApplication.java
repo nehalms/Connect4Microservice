e31546c55f3d86d0a3cbc017c0e1c2c1957864bf
nehalms <116992269+nehalms@users.noreply.github.com>
2024-11-05 14:16:36 +0530

    Initial commit after project creation

---
+++ src/main/java/com/example/Connect4Api/Connect4ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Connect4Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Connect4ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Connect4ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Connect4Api/Connect4ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Connect4Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Connect4ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
